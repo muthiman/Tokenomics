086467277a32584b93ddcdfb3e2299cc91180b2d
Muthiman <144853445+muthiman@users.noreply.github.com>
2025-05-01 18:51:46 +0100

    Create page.tsx

---
+++ app/tokenomics/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import TokenomicsSimulator from '../../TokenomicsUI';
+//  ↑ path points up twice (..) then to the file you already have.
+//    If your file is called TokenomicsUI.tsx and sits in the repo root,
+//    this import is correct.
+
+export default function Page() {
+  return <TokenomicsSimulator />;
+}
